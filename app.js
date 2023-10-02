a5ccc9ce69bf9143b63f946ae73d64efeeb43010
StruggsJ <strjac5@gmail.com>
2023-10-01 19:03:09 -0500

    inital commit

--- app.js
+++ app.js
@@ -5,6 +5,62 @@ console.log(
 
 // Exercise 1
 
+
+
+const firstName = "Jacquice"; 
+let lastName = "Struggs";
+var age = 33;
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+
 // Exercise 2
 
+// firstName = "Joseph"; - Can not reassign const values.
+lastName = "Smith";
+age = 35;
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+
 // Exercise 3
+
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log(language);
+console.log(createdYear);
+console.log(isCaseSensitive);
+
+// Exercise 4
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log(price);
+console.log(isOnSale);
+console.log(salePercentage);
+console.log(stock);
+console.log(inStock);
+console.log(selectedSize);
+
+// Exercise 5
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(title);
+console.log(author);
+console.log(pageCount);
+console.log(bookmark);
+console.log(hasRead);
